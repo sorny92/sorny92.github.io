cb13cdd40ea29667f785f3ec2b09b4d9b11e68ae
Esteve Soria <esofabian@gmail.com>
2023-04-01 22:01:35 +0200

    Add all configuration

---
+++ keyboard_keymap/falbatech_esteve/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX
